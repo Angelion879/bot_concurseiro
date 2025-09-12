8ac57c5af84d2e6c7d496d59f5be6de4dde9fbaa
Angelion879 <diangelis879@outlook.com>
2025-09-12 11:57:08 -0300

    (update) pulls json with news from first page

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import requests
 from bs4 import BeautifulSoup as bs
 
+import page_flipper as pf
+
 try:
     SITE = os.environ["ADDRESS"]
 except KeyError:
@@ -9,8 +11,9 @@ except KeyError:
     SITE = address
 
 res = requests.get(SITE)
-soup = bs(res.text, 'html.parser')
+soup = bs(res.content, 'html.parser')
+pages = soup.select('.page-link')
 
-tags = soup.select('.page-link')
+titles = soup.select('#_br_com_seatecnologia_in_buscadou_BuscaDouPortlet_params')
 
-print(tags[-2].get_text())
+print(titles)
