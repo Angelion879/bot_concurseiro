293dab658b33aa8a6ab570935da1b274518a700c
Angelion879 <diangelis879@outlook.com>
2026-02-04 12:36:13 -0300

    (feat) http request handler

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,2 +1,21 @@
 """Scrapper functions to get information from the html and json content"""
-## RE-DO
+import time
+import requests
+from bs4 import BeautifulSoup as bs
+
+def http_request(page_url):
+    """handles the http request"""
+
+    try:
+        res = requests.get(page_url, timeout=5)
+    except TimeoutError:
+        print("We got timed out! Trying again in 10s!")
+        time.sleep(10)
+        http_request(page_url)
+
+    return res
+
+
+if __name__ == '__main__':
+    response = http_request('https://blog.grancursosonline.com.br/concursos-2026/')
+    print(response.status_code)
