ca99258a0a527dabc06fb9440f4651fc61bf733f
Angelion879 <diangelis879@outlook.com>
2026-02-09 14:03:18 -0300

    (create) test file for main and boilerplate

---
+++ tests/main_test.py
@@ -0,0 +1,15 @@
+from pytest import *
+from src import scrapper, main
+
+
+class TestMain:
+    """Contains the tests fot the main file functions"""
+
+    with open('tests\\mock_site.html', 'r', encoding='utf-8') as file:
+        mock_website = file.read()
+    
+    #boilerplate
+    AREA = "TJ"
+    ROLE = "Oficial de Justiça"
+    available = scrapper.get_available_tenders(mock_website, AREA)
+    filtered = scrapper.filter_tenders_by_role(available, ROLE)
