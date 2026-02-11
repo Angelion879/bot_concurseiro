753f426ac5c7c70e87541c286c2c4de7e2ab0e0b
Angelion879 <diangelis879@outlook.com>
2026-02-11 11:14:39 -0300

    (feat) message builder

--- src/messenger.py
+++ src/messenger.py
@@ -1,4 +1,21 @@
 """Message building and sending functions"""
 
+def message_builder(content):
+    """creates message string based on the news list"""
+
+    message_content = []
+
+    for tender in content:
+        body = f"## [{tender[0]}]({tender[2]})\n{tender[1]}"
+        message_content.append(body)
+
+    message = "\n\n".join(message_content)
+
+    return message
+
 if __name__ == "__main__":
+    holder = [['Concurso TJ AL', 'Situação atual: comissão formada', 'https://exemple.com'],
+                    ['Concurso TJ PB', 'Situação atual: comissão formada', 'https://exemple.com']]
+
+    message_body = message_builder(holder)
     print("message_body")
